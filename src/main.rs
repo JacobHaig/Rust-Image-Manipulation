bb69137a14f26bff2c7f59e7c7451281880e297b
Jacob <jacobedwardhaig@gmail.com>
2021-04-23 16:57:47 -0400

    Added functions

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@
 #![allow(non_snake_case)]
 
 use image::*;
-use std::path::*;
-use std::time::Instant;
+use std::{cmp::max, path::*};
+use std::{cmp::min, time::Instant};
 use tokio::task;
 
 #[tokio::main]
 async fn main() {
+    let cur_dir = std::env::current_dir().unwrap();
     let in_folder = PathBuf::from(r"image\in");
     let out_folder = PathBuf::from(r"image\out");
 
     let start_time = Instant::now();
 
-    let files = std::fs::read_dir(in_folder)
+    if std::fs::read_dir(&in_folder).is_err() {
+        std::fs::create_dir_all(cur_dir.join(&in_folder)).unwrap();
+    }
+    if std::fs::read_dir(&out_folder).is_err() {
+        std::fs::create_dir_all(cur_dir.join(&out_folder)).unwrap();
+    }
+
+    let files = std::fs::read_dir(&in_folder)
         .expect("Folder does not exist")
         .map(|d| d.unwrap().path())
         .collect::<Vec<PathBuf>>();
@@ -55,7 +63,7 @@ fn pixel_loop1(mut loaded_image: DynamicImage) -> DynamicImage {
         for x in 0..width {
             let color: &mut Rgb<u8> = rgb.get_pixel_mut(x, y);
 
-            greyscale(color);
+            colored_shaded_levels(color);
         }
     }
 
@@ -84,12 +92,75 @@ fn brighten(img: &mut Rgb<u8>) {
     img[2] = clamp((img[2] as i16) + amount, 0, 255) as u8;
 }
 
-fn greyscale(colr: &mut Rgb<u8>) {
+fn greyscale_average(colr: &mut Rgb<u8>) {
     let avg: u8 = ((colr[0] as i16 + colr[1] as i16 + colr[2] as i16) / 3) as u8;
 
     colr[0] = avg;
     colr[1] = avg;
     colr[2] = avg;
+}
+
+fn greyscale_high_low_average(colr: &mut Rgb<u8>) {
+    let high = max(max(colr[0], colr[1]), colr[2]);
+    let low = min(min(colr[0], colr[1]), colr[2]);
+
+    let high_low_average: u8 = ((high as u16 + low as u16) / 2) as u8;
+
+    colr[0] = high_low_average;
+    colr[1] = high_low_average;
+    colr[2] = high_low_average;
+}
+
+fn desaturate(colr: &mut Rgb<u8>) {
+    use lerp::Lerp;
+    let percentage = 0.50;
+
+    let avg: f32 = ((colr[0] as i16 + colr[1] as i16 + colr[2] as i16) / 3) as f32;
+
+    colr[0] = (colr[0] as f32).lerp(avg as f32, percentage) as u8;
+    colr[1] = (colr[1] as f32).lerp(avg as f32, percentage) as u8;
+    colr[2] = (colr[2] as f32).lerp(avg as f32, percentage) as u8;
+}
+
+// The normalize function takes a value between a range of numbers and normalize
+// it between a new range of numbers. For instance, if the range is 10..20 with
+// a value of 15, and the new range is 0..100, the new value will be 50.
+fn normalize<T, I: 'static>(value: T, from_min: I, from_max: I, to_min: I, to_max: I) -> I
+where
+    T: num::cast::AsPrimitive<I>,
+    I: Copy + num::Num,
+{
+    to_min + ((value.as_() - from_min) * (to_max - to_min)) / (from_max - from_min)
+}
+
+fn greyscale_shaded_levels(colr: &mut Rgb<u8>) {
+    let num_levels: f32 = 8.;
+
+    let avg: u8 = ((colr[0] as i16 + colr[1] as i16 + colr[2] as i16) / 3) as u8;
+
+    let normal = normalize(avg as f32, 0 as f32, 255., 0., num_levels as f32) as u8;
+
+    let shaded = clamp(normal as f32 * (255. / (num_levels - 1.)), 0., 255.) as u8;
+
+    colr[0] = shaded;
+    colr[1] = shaded;
+    colr[2] = shaded;
+}
+
+fn colored_shaded_levels(colr: &mut Rgb<u8>) {
+    let num_levels: f32 = 3.;
+ 
+    let r_normal = normalize(colr[0] as f32, 0 as f32, 255., 0., num_levels as f32) as u8;
+    let g_normal = normalize(colr[1] as f32, 0 as f32, 255., 0., num_levels as f32) as u8;
+    let b_normal = normalize(colr[2] as f32, 0 as f32, 255., 0., num_levels as f32) as u8;
+
+    let r_shaded = clamp(r_normal as f32 * (255. / (num_levels - 1.)), 0., 255.) as u8;
+    let g_shaded = clamp(g_normal as f32 * (255. / (num_levels - 1.)), 0., 255.) as u8;
+    let b_shaded = clamp(b_normal as f32 * (255. / (num_levels - 1.)), 0., 255.) as u8;
+
+    colr[0] = r_shaded;
+    colr[1] = g_shaded;
+    colr[2] = b_shaded;
 }
 
 // GO VERSION For GreyScale
